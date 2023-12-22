19b93f960fcf9ffb38b1335511220a070ad7665f
abassakeem <lanreabassab1@gmail.com>
2023-12-22 17:23:32 +0100

    login register

--- JS/login.js
+++ JS/login.js
@@ -1,4 +1,4 @@
-const passwordInput = document.getElementById("password-input");
+const passwordInput = document.getElementById("password");
 
 function togglePasswordVisibility() {
     if (passwordInput.type === "password") {
@@ -8,6 +8,34 @@ function togglePasswordVisibility() {
     }
 }
 
+
+
+
 function login() {
-    
+  let username,password;
+  username = document.getElementById("username").value;
+  password = document.getElementById("password").value;
+
+  
+  let user_record = new Array();
+    user_record= JSON.parse(localStorage.getItem("users"))?JSON.parse(localStorage.getItem("users")):[]
+ 
+  if(user_record.some((v)=>{
+    return v.username==username && v.password==password
+  })){
+    alert("login successful");
+    let current_user = user_record.filter((v)=>{
+      return v.username==username && v.password==password 
+    })[0]
+// set the value of username for future use
+    localStorage.setItem("username", current_user.username);
+
+    //redirect to homepage
+    window.location.href="landing.html";
+
+  }else{
+    alert("login failed");
+  }
+
 }
+
--- JS/register.js
+++ JS/register.js
@@ -1,7 +1,7 @@
-const signupUsername = document.getElementById("signupUsername");
-const passwordInput = document.getElementById("password-input");
-const confirmPasswordInput = document.getElementById("confirm-password-input");
-const registerButton = document.querySelector(".register-button");
+// const signupUsername = document.getElementById("signupUsername");
+const passwordInput = document.getElementById("password");
+const confirmPasswordInput = document.getElementById("confirm_password");
+// const registerButton = document.querySelector(".register-button");
 
 function togglePasswordVisibility() {
     if (passwordInput.type === "password") {
@@ -20,32 +20,83 @@ function confirmPasswordVisibility() {
 }
 
 
-let users = [];
+// let users = [];
 
-function fetchDetails() {
-    //object to store the user details
-    var user = {
-        username: signupUsername.value,
-        password: passwordInput.value
-    };
+// function fetchDetails() {
+//     //object to store the user details
+//     var user = {
+//         username: signupUsername.value,
+//         password: passwordInput.value
+//     };
 
-    //push the user to the users array
-    users.push(user);
+//     //push the user to the users array
+//     users.push(user);
 
-    console.log('User signed up:', user.username);
-    console.log(users)
+//     console.log('User signed up:', user.username);
+//     console.log(users)
 
-    // Clear the form fields
-    document.querySelector('.input-fields').reset();
+//     // Clear the form fields
+//     document.querySelector('.input-fields').reset();
 
-    //redirect back to home page
-    // window.location.href = 'landing.html';
+//     //redirect back to home page
+//     // window.location.href = 'landing.html';
+// }
+
+// registerButton.addEventListener('click', () => {
+//     if (passwordInput.value !== confirmPasswordInput.value) {
+//         alert("Please confirm your password")
+//     } else {
+//         fetchDetails();
+//     }
+// })
+
+//register
+
+
+function saveData(){
+
+    let username,password,confirm_password;
+    username = document.getElementById("username").value;
+    password = document.getElementById("password").value;
+    confirm_password = document.getElementById("confirm_password").value;
+
+    
+    
+
+    // localStorage.setItem("username",username)
+    // localStorage.setItem("password",password)
+    // localStorage.setItem("confirm_password",confirm_password)
+
+
+
+
+    //array to store registered users
+
+    let user_records = new Array();
+    user_records= JSON.parse(localStorage.getItem("users"))?JSON.parse(localStorage.getItem("users")):[]
+    if(user_records.some((v)=>{
+        return v.username==username
+    })){
+            alert("Username already exits")
+    }
+    else{
+        user_records.push(
+            {
+                "username":username,
+                "password":password,
+                "confirm_password": confirm_password
+            }
+        )
+
+        //store the user data in localstorage
+        // stringify converts objects to string
+        localStorage.setItem("users",JSON.stringify(user_records));
+    }
 }
 
-registerButton.addEventListener('click', () => {
-    if (passwordInput.value !== confirmPasswordInput.value) {
-        alert("Please confirm your password")
-    } else {
-        fetchDetails();
-    }
-})
+
+
+
+
+
+
